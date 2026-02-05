999a01b93a4fb8b2cdba038a366c8339b4719951
Anshika2194 <srianshika2194@gmail.com>
2026-02-05 17:33:57 +0530

    Add all project files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>
+        app deploy ec2
+      </h1>
+      <h2>app deploy ec2</h2>
+      <h3>this is practise</h3>
+      <h4>practise</h4>
+    </div>
+  )
+}
+
+export default App
